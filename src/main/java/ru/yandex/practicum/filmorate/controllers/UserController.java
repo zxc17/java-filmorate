a2c995bca3255d4cd246f2a3a548e2afe062496e
Andrey Zaraev <zxc-17@yandex.ru>
2022-06-30 18:17:59 +0500

    Сделать базовую реализацию, без тестов.

---
+++ src/main/java/ru/yandex/practicum/filmorate/controllers/UserController.java
@@ -0,0 +1,63 @@
+package ru.yandex.practicum.filmorate.controllers;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
+import ru.yandex.practicum.filmorate.customExceptions.ValidationException;
+import ru.yandex.practicum.filmorate.model.User;
+
+import java.time.LocalDate;
+import java.util.*;
+import java.util.stream.Collectors;
+
+@RestController
+@Slf4j
+public class UserController {
+    Map<Integer, User> users = new HashMap<>();
+    private int id = 1;
+
+    @GetMapping("/users")
+    public List<User> getUser(){
+        return new ArrayList<>(users.values());
+    }
+
+    @PostMapping("/users")
+    public User addUser(@RequestBody User u) {
+        if (!checkUser(u)) {
+            log.warn("Некорректные данные пользователя.");
+            throw new ValidationException("Некорректные данные пользователя.");
+        }
+        u.setId(id++);
+        users.put(u.getId(), u);
+        log.info("Пользователь id={} успешно добавлен.", u.getId());
+        return u;
+    }
+
+    @PutMapping("/users")
+    public User updateUser(@RequestBody User u) {
+        if (!checkUser(u)) {
+            log.warn("Некорректные данные пользователя.");
+            throw new ValidationException("Некорректные данные пользователя.");
+        }
+        if (!users.containsKey(u.getId())) {
+            log.warn("Невозможно обновить данные пользователя, ID не найден.");
+            throw new ValidationException("Невозможно обновить данные пользователя, ID не найден.");
+        }
+        users.put(u.getId(), u);
+        log.info("Данные пользователя id={} успешно обновлены.", u.getId());
+        return u;
+    }
+
+    private boolean checkUser(User u) {
+        if (u == null ||
+                u.getEmail().isBlank() || !u.getEmail().contains("@") ||
+                u.getLogin().isBlank() || u.getLogin().contains(" ") ||
+                u.getBirthday().isAfter(LocalDate.now())
+        )
+            return false;
+        else {
+            if (u.getName().isBlank())
+                u.setName(u.getLogin());
+            return true;
+        }
+    }
+}
--- src/test/java/ru/yandex/practicum/filmorate/FilmorateApplicationTests.java
+++ src/test/java/ru/yandex/practicum/filmorate/FilmorateApplicationTests.java
@@ -1,13 +1,30 @@
 package ru.yandex.practicum.filmorate;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootTest
 class FilmorateApplicationTests {
+	ConfigurableApplicationContext application;
+
+	@BeforeEach
+	private void start() {
+		application = SpringApplication.run(FilmorateApplication.class);
+	}
+
+	@AfterEach
+	private void stop() {
+		int exitCode = SpringApplication.exit(application, () -> 0);
+		System.exit(exitCode);
+	}
 
 	@Test
-	void contextLoads() {
+	void UserCreateFailLogin() {
+
 	}
 
 }
