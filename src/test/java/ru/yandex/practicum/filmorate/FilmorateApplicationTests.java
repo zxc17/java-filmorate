f78469ffea2c57c51a57a6619b980f859c9ffeb1
Andrey Zaraev <zxc-17@yandex.ru>
2022-07-01 11:32:53 +0500

    Сдать Спринт-8.

--- src/test/java/ru/yandex/practicum/filmorate/FilmorateApplicationTests.java
+++ src/test/java/ru/yandex/practicum/filmorate/FilmorateApplicationTests.java
@@ -1,30 +1,13 @@
 package ru.yandex.practicum.filmorate;
 
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootTest
 class FilmorateApplicationTests {
-	ConfigurableApplicationContext application;
-
-	@BeforeEach
-	private void start() {
-		application = SpringApplication.run(FilmorateApplication.class);
-	}
-
-	@AfterEach
-	private void stop() {
-		int exitCode = SpringApplication.exit(application, () -> 0);
-		System.exit(exitCode);
-	}
 
 	@Test
-	void UserCreateFailLogin() {
-
+	void contextLoads() {
 	}
 
 }
